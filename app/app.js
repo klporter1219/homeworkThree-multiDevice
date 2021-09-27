b5eaadc4266442ca0399eae4a43fa49b24fabe9d
klporter1219 <78804731+klporter1219@users.noreply.github.com>
2021-09-26 21:27:41 -0400

    Initial Push

---
+++ app/app.js
@@ -0,0 +1,17 @@
+
+
+function route() {
+    let hashTAG = window.location.hash;
+    let id = hashTAG.replace("#/", "");
+
+    MODEL.getPages(id);
+}
+
+function initSite() {
+    $(window).on('hashchange', route);
+    route();
+}
+
+$(document).ready(function (){
+   initSite();
+});
